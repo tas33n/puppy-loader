2bad6fd26d6add6982927581414679cf5e0702c3
Tas33n <55633040+tas33n@users.noreply.github.com>
2023-09-26 14:24:09 +0600

    Update puppy.js

--- puppy.js
+++ puppy.js
@@ -1,50 +1,37 @@
 const express = require('express');
-const puppeteer = require('puppeteer-extra');
-const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const puppeteer = require('puppeteer');
+
 const app = express();
-// const port = 3000;
 const port = process.env.PORT || 5001;
 
-// Use StealthPlugin to bypass some anti-bot measures
-puppeteer.use(StealthPlugin());
-
-app.get('/api', async (req, res) => {
+// Define a route to fetch and display a web page
+app.get('/p', async (req, res) => {
   try {
-    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
-    const page = await browser.newPage();
-
-    // Set the user agent to mimic a real browser
-    await page.setUserAgent(
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36'
-    );
-
-    // Get the URL parameter from the request
-    const url = req.query.url;
+    const { url } = req.query;
 
     if (!url) {
-      return res.status(400).send('URL parameter is missing');
+      return res.status(400).send('Missing URL parameter.');
     }
 
-    // Navigate to the specified URL
-    await page.goto(url, { waitUntil: 'networkidle2' });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    const page = await browser.newPage();
 
-    // Wait for the page to load completely (you can adjust the wait time as needed)
-    await page.waitForTimeout(5000);
+    await page.goto(url);
 
     // Get the HTML content of the page
-    const htmlContent = await page.content();
+    const pageContent = await page.content();
 
-    // Close the browser
     await browser.close();
 
-    // Respond with the HTML content
-    res.send(htmlContent);
+    // Display the page content in the browser
+    res.send(pageContent);
   } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('An error occurred');
+    console.error(error);
+    res.status(500).send('An error occurred while fetching and displaying the page.');
   }
 });
 
+// Start the Express.js server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
